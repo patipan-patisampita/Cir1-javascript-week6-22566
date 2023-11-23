641d7bdb1d6510e29a326616b7330daef8d9a4c9
patipan-patisampita <patipan-patisampita@gmail.com>
2023-11-23 15:14:37 +0700

    update

---
+++ app103.js
@@ -0,0 +1,28 @@
+//3.Array constructor method
+console.log("3.Array constructor method");
+const num3 = new Array(5)
+num3[0] = "Trat1";
+num3[1] = "Trat2";
+num3[2] = "Trat3";
+num3[3] = "Trat4";
+num3[4] = "Trat5";
+num3.push("Chonburi");
+for (let i = 0; i < num3.length; i = i + 1) {
+  console.log(num3[i]);
+}
+//1.Literal array
+console.log("1.Array Literal");
+const num2 = ["trat", "chantaburi", "Rayong"];
+num2.push("Chonburi")
+num2[4] = "Bangkok";
+for (let i = 0; i < num2.length; i = i + 1) {
+  console.log(num2[i]);
+}
+console.log("2.Array Instance method");
+//2.Array Instance method
+const num = Array(1,2,3,4,5,"trat",20.5);
+for (let i = 0; i < num.length; i = i + 1) {
+  console.log(num[i]);
+}
+
+
